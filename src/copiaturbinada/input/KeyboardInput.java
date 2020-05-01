d6f9244f77b4dfe02725cec2936f8650b63b0d82
Caio <csbc326@gmail.com>
2020-05-01 16:11:55 -0300

    :bug: Add more lines to keyboard input

--- src/copiaturbinada/input/KeyboardInput.java
+++ src/copiaturbinada/input/KeyboardInput.java
@@ -6,12 +6,23 @@ public class KeyboardInput implements Input{
 
 	@Override
 	public String input() {
-		String input;
-		
+		String input = "";
 		Scanner keyboard = new Scanner(System.in);
 		
-		input = keyboard.nextLine();
+		System.out.println("Write what you want to copy below "
+				+ "(After writing, create a new line with _@end_ written, to end input)");
+		System.out.println("KEYBOARD INPUT:");
+		System.out.println("----------------------");
 		
+		while (true) {
+			String nextLine = keyboard.nextLine();
+			if (nextLine.contentEquals("_@end_")) {
+				break;
+			}
+			input += nextLine + System.lineSeparator();
+		}
+		
+		System.out.println("----------------------");
 		keyboard.close();
 		return input;
 	}
--- src/copiaturbinada/output/ScreenOutput.java
+++ src/copiaturbinada/output/ScreenOutput.java
@@ -4,7 +4,10 @@ public class ScreenOutput implements Output {
 
 	@Override
 	public void output(String input) {
+		System.out.println("SCREEN OUTPUT:");
+		System.out.println("----------------------");
 		System.out.print(input);
+		System.out.println("----------------------");
 	}
 
 }
